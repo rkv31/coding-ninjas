dd3e8f674601cf90fe58dc24640ac2385687ba5e
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:30:02 +0530

    Update Running Median.cpp

--- lecture 15- Priority Queues/Running Median.cpp
+++ lecture 15- Priority Queues/Running Median.cpp
@@ -1,3 +1,32 @@
+Running median
+You are given a stream of n integers. For every ith integer, add it to the running list of integers and print the resulting median (of the list till now).
+Print the only integer part of median.
+Input Format :
+Line 1 : An integer N denoting the number of integers in the stream
+Line 2 : N integers which are separated by spaces
+Output Format :
+After each new integer is added to the list print the updated median on a new line
+Input Constraints
+1 <= n <= 10^5
+1 <= Ai <= 10^5
+Sample Input 1 :
+6
+6 2 1 3 7 5
+Sample Output 1 :
+6
+4
+2
+2
+3
+4
+Sample Output explanation :
+S = {6}, median = 6
+S = {6, 2} -> {2, 6}, median = 4
+S = {6, 2, 1} -> {1, 2, 6}, median = 2
+S = {6, 2, 1, 3} -> {1, 2, 3, 6}, median = 2
+S = {6, 2, 1, 3, 7} -> {1, 2, 3, 6, 7}, median = 3
+S = {6, 2, 1, 3, 7, 5} -> {1, 2, 3, 5, 6, 7}, median = 4
+
 #include<queue>
 using namespace std;
 void findMedian(int arr[], int n){
