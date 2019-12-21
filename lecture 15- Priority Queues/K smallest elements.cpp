e67a4c6af7313566a72d92dc32f648c9c543a564
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:25:48 +0530

    Update K smallest elements.cpp

--- lecture 15- Priority Queues/K smallest elements.cpp
+++ lecture 15- Priority Queues/K smallest elements.cpp
@@ -1,5 +1,4 @@
 Code : K smallest Elements
-Send Feedback
 You are given with an integer k and an array of integers that contain numbers in random order. Write a program to find k smallest numbers from given array. You need to save them in an array and return it.
 Time complexity should be O(nlogk) and space complexity should be not more than O(k).
 Order of elements in the output is not important.
