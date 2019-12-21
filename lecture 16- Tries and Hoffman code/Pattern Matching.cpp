554d1321cc4f246c7cd0de3dca1503d5332bccc3
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:33:00 +0530

    Update Pattern Matching.cpp

--- lecture 16- Tries and Hoffman code/Pattern Matching.cpp
+++ lecture 16- Tries and Hoffman code/Pattern Matching.cpp
@@ -1,3 +1,31 @@
+Pattern Matching
+Given a list of n words and a pattern p that we want to search. Check if the pattern p is present the given words or not.
+Return true or false.
+Input Format :
+Line 1 : Integer n
+Line 2 : n words (separated by space)
+Line 3 : Pattern p (a string)
+Output Format :
+true ot false
+Sample Input 1 :
+4
+abc def ghi cba
+de
+Sample Output 2 :
+true
+Sample Input 2 :
+4
+abc def ghi hg
+hi
+Sample Output 2 :
+true
+Sample Input 3 :
+4
+abc def ghi hg
+hif
+Sample Output 3 :
+false
+
 // #include "TrieNode.h"
 #include <string>
 #include <vector>
