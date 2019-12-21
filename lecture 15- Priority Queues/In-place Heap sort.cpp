e1ce1dff95aacec679f35fd952873d09c1caa334
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:24:18 +0530

    Update In-place Heap sort.cpp

--- lecture 15- Priority Queues/In-place Heap sort.cpp
+++ lecture 15- Priority Queues/In-place Heap sort.cpp
@@ -1,4 +1,20 @@
-
+Code : In-place heap sort
+Given an integer array of size n. Sort this array (in decreasing order) using heap sort.
+Space complexity should be O(1).
+Input Format :
+Line 1 : Integer n, Array size
+Line 2 : Array elements, separated by space
+Output Format :
+Array elements after sorting
+Constraints :
+1 <= n <= 10^6
+Sample Input:
+6 
+2 6 8 5 4 3
+Sample Output:
+8 6 5 4 3 2
+    
+    
 void inplaceHeapSort(int input[], int n){
     /* Don't write main().
      * Don't read input, it is passed as function argument.
