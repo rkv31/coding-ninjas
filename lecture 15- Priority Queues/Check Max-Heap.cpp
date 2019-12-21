9436550de0652a522ad027d8bf2490df09f62c13
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:27:11 +0530

    Update Check Max-Heap.cpp

--- lecture 15- Priority Queues/Check Max-Heap.cpp
+++ lecture 15- Priority Queues/Check Max-Heap.cpp
@@ -1,3 +1,16 @@
+Check Max-Heap
+Given an array of integers, check whether it represents max-heap or not.
+Return true or false.
+Input Format :
+Line 1 : An integer N i.e. size of the array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+true if it represents max-heap and false if it is not a max-heap
+Constraints :
+1 <= N <= 10^5
+1 <= Ai <= 10^5
+
+
 bool checkMaxHeap(int arr[], int n){
     /* Don't write main().
      * Don't read input, it is passed as function argument.
