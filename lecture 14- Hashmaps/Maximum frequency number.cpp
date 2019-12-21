905d5fa1a01c36f50d3377a5ceb4dff34d02e741
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:20:04 +0530

    Update Maximum frequency number.cpp

--- lecture 14- Hashmaps/Maximum frequency number.cpp
+++ lecture 14- Hashmaps/Maximum frequency number.cpp
@@ -1,3 +1,25 @@
+Code : Maximum Frequency Number
+You are given with an array of integers that contain numbers in random order. Write a program to find and return the number which occurs maximum times in the given input.
+If more than one element occurs same number of times in the input, return the element which is present in the input first.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+Most frequent element
+Constraints :
+1 <= N <= 10^5
+Sample Input 1 :
+13
+2 12 2 11 12 2 1 2 2 11 12 2 6 
+Sample Output 1 :
+2
+Sample Input 2 :
+3
+1 4 5
+Sample Output 2 :
+1
+
+
 #include<unordered_map>
 #include<climits>
 	int highestFrequency(int *input, int n){	 
