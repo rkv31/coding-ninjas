5519417774ff93e117a34b419c019be8dd051e50
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:23:20 +0530

    Update Max Priority Queue.cpp

--- lecture 15- Priority Queues/Max Priority Queue.cpp
+++ lecture 15- Priority Queues/Max Priority Queue.cpp
@@ -1,4 +1,18 @@
+Code : Max Priority Queue
+Implement the class for Max Priority Queue which includes following functions -
+1. getSize -
+Return the size of priority queue i.e. number of elements present in the priority queue.
+2. isEmpty -
+Check if priority queue is empty or not. Return true or false accordingly.
+3. insert -
+Given an element, insert that element in the priority queue at the correct position.
+4. getMax -
+Return the maximum element present in the priority queue without deleting. Return -Infinity if priority queue is empty.
+5. removeMax -
+Delete and return the maximum element present in the priority queue. Return -Infinity if priority queue is empty.
+Note : main function is given for your reference which we are using internally to test the class.
 
+ 
 /***************
  * Following is the main function that er are using internally
  *
