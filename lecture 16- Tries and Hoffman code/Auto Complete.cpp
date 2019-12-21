44a16cf9378ec1e4c435a75b5ef3415d74b40aee
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:34:49 +0530

    Update Auto Complete.cpp

--- lecture 16- Tries and Hoffman code/Auto Complete.cpp
+++ lecture 16- Tries and Hoffman code/Auto Complete.cpp
@@ -1,3 +1,37 @@
+Auto complete
+Givan n number of words and an incomplete word w. You need to auto-complete that word w.
+That means, find and print all the possible words which can be formed using the incomplete word w.
+Input Format :
+Line 1 : Integer n
+Line 2 : n words (separated by space)
+Line 3 : Word w
+Output Format :
+All possible words (in different lines)
+Note : Order of words doesn't matter.
+Sample Input 1 :
+7
+do dont no not note notes den
+no
+Sample Output 2 :
+no
+not
+note
+notes
+Sample Input 2 :
+7
+do dont no not note notes den
+de
+Sample Output 2 :
+den
+Sample Input 3 :
+7
+do dont no not note notes den
+nom
+Sample Output 3 :
+(Empty) There is no word which starts with "nom"
+	
+	
+
 // #include "TrieNode.h"
 #include <string>
 #include <vector>
