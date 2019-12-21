7c18d61f2c2a55176f5c743acd0072f7262f0825
rkv31 <58583307+rkv31@users.noreply.github.com>
2019-12-22 01:22:00 +0530

    Update Remove Min.cpp

--- lecture 15- Priority Queues/Remove Min.cpp
+++ lecture 15- Priority Queues/Remove Min.cpp
@@ -1,3 +1,11 @@
+Code : Remove Min
+Send Feedback
+Implement the function RemoveMin for the min priority queue class.
+For a minimum priority queue, write the function for removing the minimum element present. Remove and return the minimum element.
+Note : main function is given for your reference which we are using internally to test the code.
+
+    
+    
 #include <vector>
 
 /*******************
